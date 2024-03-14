8920c28a0d53d843f0c76bfe77fc060b2cd7bba7
Constantine <setwindowtext@gmail.com>
2024-03-14 15:17:15 +0100

    Fixing a bug with getting GitHub release info

--- themes/hugo-story/assets/js/github.js
+++ themes/hugo-story/assets/js/github.js
@@ -3,11 +3,12 @@
 		.then(resp => resp.json())
 		.then(json => {
 			const release = json['name'];
-			$(`#downloads-title`).text(`Download Flowkeeper ${release}`);
+			const when = json['published_at'].split('T')[0];
+			$(`#downloads-title`).text(`Download Flowkeeper ${release} (${when})`);
 			const notes = json['body'];
 			json['assets'].forEach(asset => {
 				const url = asset['browser_download_url'];
-				const name = asset['name'];
+				const name = asset['name'].replaceAll(' ', '-').replaceAll('.', '-').toLowerCase();
 				const size = asset['size'];
 				const count = asset['download_count'];
 				$(`#link-${name}`).href(url);
