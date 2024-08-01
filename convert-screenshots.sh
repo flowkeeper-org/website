83af1131938397ab427b5c67c2d0f582ea277c7e
Constantine <setwindowtext@gmail.com>
2024-08-01 16:45:57 +0200

    Regenerated screenshots for 0.6.2

--- convert-screenshots.sh
+++ convert-screenshots.sh
@@ -3,7 +3,7 @@
 sitedir=$(pwd)
 cd ~/projects/fk-desktop/test-results
 
-for original in *.png; do
+for original in *-full.png; do
 	filename="${original%.*}"
 	full="$sitedir/static/images/gallery/fulls/$filename.jpg"
 	convert "$original" -crop 1500x980+200+50 -quality 90 "$full"
