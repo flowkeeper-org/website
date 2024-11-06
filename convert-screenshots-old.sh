3efdf39e16e9a5ff3b801b8e4677263ea68113a3
Constantine <setwindowtext@gmail.com>
2024-11-06 15:39:08 +0100

    Updated screenshots with v0.8.0

---
+++ convert-screenshots-old.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+sitedir=$(pwd)
+cd ~/projects/fk-desktop/test-results
+
+for original in *-full.png; do
+	filename="${original%.*}"
+	full="$sitedir/static/images/gallery/fulls/$filename.jpg"
+	convert "$original" -crop 1500x980+200+50 -quality 90 "$full"
+	thumb="$sitedir/static/images/gallery/thumbs/$filename.jpg"
+	convert "$original" -crop 1500x980+200+50 -resize 50% -quality 90 "$thumb"
+done
+
