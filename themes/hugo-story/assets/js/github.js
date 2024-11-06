074f88408b0a95cbe8be31be811ea0d2d12f05bd
Constantine <setwindowtext@gmail.com>
2024-11-06 17:36:42 +0100

    Added what's new section + cleanup

--- themes/hugo-story/assets/js/github.js
+++ themes/hugo-story/assets/js/github.js
@@ -6,6 +6,7 @@
 			const when = json['published_at'].split('T')[0];
 			$(`#downloads-title`).text(`Download Flowkeeper ${release}`);
 			$(`#downloads-subtitle`).text(`Released on ${when}`);
+			$(`#downloads-whatsnew`).attr('href', `/${release}`);
 			const notes = json['body'];
 			json['assets'].forEach(asset => {
 				const url = asset['browser_download_url'];
