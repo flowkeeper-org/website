3056d5189cbd07f9bae7ccf368ef8571b588f565
co-stig <setwindowtext@gmail.com>
2024-12-29 10:18:59 +0100

    Updated screenshot converter and image gallery structure

--- convert-screenshots-mac.sh
+++ convert-screenshots-mac.sh
@@ -1,22 +1,70 @@
 #!/usr/bin/env bash
 
 sitedir=$(pwd)
+release=$1
+os=$(uname)
+
+if [ -z "$release" ]; then
+	echo "Fatal: Specify a release, e.g. v0.9.0"
+	exit 1
+fi
+
+outbase="$sitedir/static/images/releases/$release/$os"
+echo "Will output screenshots to $outbase"
+
+fulls="$outbase/fulls"
+mkdir -p $fulls
+
+thumbs="$outbase/thumbs"
+mkdir -p $thumbs
+
 cd ~/projects/fk-desktop/test-results
+
+getcrop() {
+	local original=$1
+    if [ -x "$(command -v magick)" ]; then
+		size=$(magick identify -ping -format "%[w]x%[h]" "$original")
+	else
+		size=$(identify -ping -format "%[w]x%[h]" "$original")
+	fi
+	if [ "$size" = "4480x2520" ]; then
+		crop="3840x2160+320+180"
+	elif [ "$size" = "1920x1080" ]; then
+		crop="1500x980+200+50"
+	else
+		crop="$size+0+0"
+	fi
+	echo "   - Will crop from $size to $crop"
+}
+
+convert() {
+	local original=$1
+	local outfile=$2
+	local resize=$3
+
+	getcrop "$original"
+	
+	out_png="$outfile.png"
+    if [ -x "$(command -v magick)" ]; then
+		magick "$original" -crop "$crop" -resize "$resize" -unsharp 0x1 "$out_png"
+		echo "   - Saved PNG using magick: $out_png"
+		magick "$out_png" -quality 90 "$outfile"
+		echo "   - Saved JPG using magick: $outfile"
+	else
+		convert "$original" -crop "$crop" -resize "$resize" -unsharp 0x1 "$out_png"
+		echo "   - Saved PNG using convert: $out_png"
+		convert "$out_png" -quality 90 "$outfile"
+		echo "   - Saved JPG using convert: $outfile"
+    fi
+	rm "$out_png"
+	echo "   - Deleted PNG"
+}
 
 for original in *-full.png; do
 	filename="${original%.*}"
-	echo "Processing $filename"
-
-	full_png="$sitedir/static/images/gallery/fulls/$filename.png"
-	magick "$original" -crop 3840x2160+320+180 -resize 50% -unsharp 0x1 "$full_png"
-	full_jpg="$sitedir/static/images/gallery/fulls/$filename.jpg"
-	magick "$full_png" -quality 90 "$full_jpg"
-	rm "$full_png"
-
-	thumb_png="$sitedir/static/images/gallery/thumbs/$filename.png"
-	magick "$original" -crop 3840x2160+320+180 -resize 25% -unsharp 0x1 "$thumb_png"
-	thumb_jpg="$sitedir/static/images/gallery/thumbs/$filename.jpg"
-	magick "$thumb_png" -quality 90 "$thumb_jpg"
-	rm "$thumb_png"
+	echo "Processing $filename:"
+	echo " - Full-sized screenshot"
+	convert "$original" "$fulls/$filename.jpg" "1920x1080"
+	echo " - Thumbnail screenshot"
+	convert "$original" "$thumbs/$filename.jpg" "960x540"
 done
-
