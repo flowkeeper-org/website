e54ecc0eda4a08a9be36c753f3ebaff140a44bd7
co-stig <setwindowtext@gmail.com>
2025-04-18 20:20:48 +0200

    Updated downloads

--- themes/hugo-story/assets/js/github.js
+++ themes/hugo-story/assets/js/github.js
@@ -11,12 +11,14 @@
 		moreTable.hide();
 	});
 
-	// fetch("https://api.github.com/repos/flowkeeper-org/fk-desktop/releases/latest")
+	//fetch("https://api.github.com/repos/flowkeeper-org/fk-desktop/releases/latest")
 	fetch("https://api.github.com/repos/flowkeeper-org/fk-desktop/releases/207595463")
 		.then(resp => resp.json())
 		.then(json => {
 			const release = json['name'];
 			const version = json['tag_name'].replace('v', '');
+			const assetPrefix = 'flowkeeper-' + version.replaceAll('.', '-') + '-';
+			console.log('version', assetPrefix);
 			const when = json['published_at'].split('T')[0];
 			const dict = {};
 			$(`#downloads-title`).text(`Download Flowkeeper ${release}`);
@@ -26,14 +28,15 @@
 			json['assets'].forEach(asset => {
 				const url = asset['browser_download_url'];
 				const name = asset['name'].replaceAll(' ', '-').replaceAll('.', '-').toLowerCase();
+				const elementSuffix = name.replaceAll(assetPrefix, '');
 				const size = asset['size'];
 				const count = asset['download_count'];
-				$(`#link-${name}`).attr('href', url);
+				$(`#link-${elementSuffix}`).attr('href', url);
 				let sizeText = `${Math.round(size / 1024 / 1024)}MB`;
 				if (count > 1000) {
 					sizeText += `, ${count} downloads`;
 				}
-				$(`#size-${name}`).text(`(${sizeText})`);
+				$(`#size-${elementSuffix}`).text(`(${sizeText})`);
 				dict[asset['name'].replace(`flowkeeper-${version}`, 'download-more-link')] = {'url': url, 'size': sizeText, 'count': count};
 			});
 			console.log('Received', dict);
