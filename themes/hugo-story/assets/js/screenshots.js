89918b4d4c83f9fd0135eb6ac14bcf25827c55a0
Constantine <setwindowtext@gmail.com>
2025-06-14 23:00:33 +0200

    Autodetect OS for screenshots

--- themes/hugo-story/assets/js/screenshots.js
+++ themes/hugo-story/assets/js/screenshots.js
@@ -1,3 +1,65 @@
+
+/**
+ * Based on https://www.html-code-generator.com/javascript/detect-browser
+ */
+const getOS = () => {
+    const ua = navigator.userAgent;
+
+    const osMatchers = [
+        {
+            name: 'Windows',
+            regex: /Windows NT ([0-9.]+)/,
+        },
+        {
+            name: 'MacOS',
+            regex: /Mac OS X ([0-9._]+)/,
+        },
+        {
+            name: 'iOS',
+            regex: /OS ([\d._]+) like Mac OS X/,
+        },
+        {
+            name: 'iPadOS',
+            regex: /iPad.*OS ([\d._]+)/,
+        },
+        { name: 'Android', regex: /Android ([0-9.]+)/ },
+        { name: 'FreeBSD', regex: /FreeBSD/ },
+        { name: 'OpenBSD', regex: /OpenBSD/ },
+        { name: 'NetBSD', regex: /NetBSD/ },
+        { name: 'Solaris', regex: /SunOS/ },
+        {
+            name: 'Linux',
+            regex: /Linux/,
+        },
+        {
+            name: 'Chrome OS',
+            regex: /CrOS/
+        }
+    ];
+
+    for (const os of osMatchers) {
+        const match = ua.match(os.regex);
+        if (match) {
+            switch (os.name) {
+                case 'MacOS':
+                case 'iOS':
+                case 'iPadOS':
+                    return 'macos';
+                case 'Linux':
+                case 'FreeBSD':
+                case 'OpenBSD':
+                case 'NetBSD':
+                case 'Solaris':
+                    return 'linux';
+                default:
+                    return 'windows';	// The safest default given the number of downloads
+            }
+        }
+    }
+
+    return 'windows';
+};
+
 (async function ($) {
 
 	function getGalleries() {
@@ -46,4 +108,5 @@
 	
 	getGalleries().each(attachListener);
 
+	$(`#gallery-${getOS()}`).click();
 })(jQuery);
